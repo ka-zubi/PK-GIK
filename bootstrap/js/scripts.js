d059cfac9f54aa1644802e7f60c4c2494e3ade1c
KACPER\kzube <rivehajs@gmail.com>
2024-10-27 15:50:43 +0100

    Dodanie tabeli i cssów

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Strona została w pełni załadowana.');
 
-    
+    // Przykład dodania dynamicznego zachowania
     const alertElement = document.querySelector('.alert');
     if (alertElement) {
         alertElement.addEventListener('closed.bs.alert', function () {
